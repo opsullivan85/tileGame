0f9f18d0a74d9da360238643b11d7b3cbc778475
opsullivan85 <opsullivan85@gmail.com>
2022-12-26 14:01:59 -0800

    Made run.bat smarter

--- run.bat
+++ run.bat
@@ -1,3 +1,10 @@
-call venv/Scripts/activate.bat
-python -m pip install -r requirements.txt
-python -m game
+
+if exist "venv\" (
+    call venv/Scripts/activate.bat
+    python -m game
+) else (
+    python -m venv venv
+    call venv/Scripts/activate.bat
+    python -m pip install -r requirements.txt
+    python -m game
+)
